f87462ce8a11fef2338910fe8886cf5180cca210
Melanie Fabian <melanieblogged1@gmail.com>
2022-04-20 12:30:51 +0000

    hot fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
 int main(void)
-	/*prints the string using the puts function with backslash and quotation*/
+	/**
+	 * prints the string using the puts function with backslash and quotation
+	 */
 
 {
 	puts("\"Programming is like building a multilingual puzzle");
