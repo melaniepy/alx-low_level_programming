dcd603b8917ce3c7814f425bd873d64dead6c1d1
Melanie Fabian <melanieblogged1@gmail.com>
2022-04-20 16:47:36 +0000

    hot fix

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
-/** main - prints the string with the printf function
+
+/**
+*
+* main - prints the string with the printf function
 *
 * Return: 0
 */
 
 int main(void)
+
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 
