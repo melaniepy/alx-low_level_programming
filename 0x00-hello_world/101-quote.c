f5c1e0f22b066fb10f04a72bd0da630a8ec08d92
Melanie Fabian <melanieblogged1@gmail.com>
2022-04-21 00:27:47 +0000

    6-size.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+*main - prints the string with the function fwrite
+*
+*Return: 0
+*/
+
+int main(void)
+{
+	fwrite(\"and that piece of art is useful" - Dora Korpar, 2015-10-19);
+	return 0;
+}
