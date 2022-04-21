99608dcf87e387b4bdb91121d82adf8fcbe07eca
Melanie Fabian <melanieblogged1@gmail.com>
2022-04-21 00:29:59 +0000

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,6 +13,6 @@ int main(void)
 	printf("Size of a long int: %d byte(s)\n", sizeof(long));
 	printf("Size of a long long int: %d byte(s)\n", sizeof(long long));
 	printf("Size of a float: %d byte(s)\n", sizeof(float));
-	
+
 	return (0);
 }
