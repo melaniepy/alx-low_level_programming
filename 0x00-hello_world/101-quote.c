b7094a4d82eadc9d1462cf694c2547dc78ca789a
Melanie Fabian <melanieblogged1@gmail.com>
2022-04-22 14:39:02 +0000

    commited this file

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 
-/**
-*main - prints the string with the function fwrite
-*
-*Return: 0
-*/
-
 int main(void)
+
+/**
+* main - prints the string using the putchar function
+*
+* Return: 0
+*
+*/
+ 
 {
-	fwrite(\"and that piece of art is useful" - Dora Korpar, 2015-10-19);
-	return 0;
+	 putchar("\and that piece of art is useful" - Dora Korpar, 2015-10-19);
+
+	 return (0);
 }
