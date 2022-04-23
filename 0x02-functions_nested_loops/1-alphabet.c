3b49343305cdc3ebedf2d7fc4c37e6a2ffbb5e88
melaniepy <101869476+melaniepy@users.noreply.github.com>
2022-04-23 16:50:53 +0100

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,18 @@
-kbiusdj
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabet
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+	_putchar('\n');
+}
