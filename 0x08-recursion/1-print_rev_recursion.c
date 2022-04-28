675d5679c6d3c99926ec607d51e2b9e34db99651
melaniepy <101869476+melaniepy@users.noreply.github.com>
2022-04-28 17:54:52 +0100

    Update 1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,1 +1,16 @@
-kjddifhdf
+#include "main.h"
+
+/**
+ * _print_rev_recursion -reverse
+ * @s: pointer to string parameters
+ *
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
