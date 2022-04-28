81f614a98e9eac34317e7f207aeef71bb4df7f1f
melaniepy <101869476+melaniepy@users.noreply.github.com>
2022-04-28 18:18:42 +0100

    Update 4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,1 +1,22 @@
-kjifdhugjdsf
+#include "main.h"
+
+/**
+ * _pow_recursion - pow recursion
+ * @x: integer parameter
+ * @y: integer parameter
+ * Return: recursion
+ */
+
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+	return (x * _pow_recursion(x, y - 1));
+}
