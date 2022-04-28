609d0f4d103dd570a71941da62bec9e34c783a90
melaniepy <101869476+melaniepy@users.noreply.github.com>
2022-04-28 18:14:49 +0100

    Update 3-factorial.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,1 +1,20 @@
-ksjisduhf
+#include "main.h"
+
+/**
+ * factorial - factorial n
+ * @n: integer parameters
+ * Return: recursion
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 1)
+	{
+		return (1);
+	}
+	return (n * factorial(n - 1));
+}
