e9c579435b8fc44298780cd4796e42071a6ef105
melaniepy <101869476+melaniepy@users.noreply.github.com>
2022-05-10 17:56:37 +0100

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,1 +1,17 @@
-mhvdsdy
+#include <stdio.h>
+
+/**
+ * main - is a function that prints the name.
+ * @argc: is an argument counter for char.
+ * @argv: is an agument value for char.
+ * Return: Returns always success.
+ */
+
+int main(int argc, char *argv[])
+{
+	int i = argc - argc;
+
+	printf("%s\n", argv[i]);
+
+	return (0);
+}
