a1d1ab4f6521c49d2537645bcd77e9e0df5b339e
melaniepy <101869476+melaniepy@users.noreply.github.com>
2022-05-10 16:42:20 +0100

    Update main.h

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,20 +1,12 @@
-alx-low_level_programming/0x0B-malloc_free/main.h
-@melaniepy
-melaniepy malloc protoype fuctions
-Latest commit a977f77 on Apr 5
- History
- 1 contributor
-Executable File  12 lines (10 sloc)  297 Bytes
-   
-#ifndef _MAIN_H_
-#define _MAIN_H_
-
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+int _putchar(char c);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
-char **strtow(char *str);
-
 #endif
